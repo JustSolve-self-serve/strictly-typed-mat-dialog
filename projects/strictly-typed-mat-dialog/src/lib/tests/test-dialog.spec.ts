e1eb18c6a5fd1397d32182970c2d361fcb76d1d3
Francesco Manicardi <francesco.manicardi@just-solve.com>
2025-04-08 11:27:51 +0200

    add tests

---
+++ projects/strictly-typed-mat-dialog/src/lib/tests/test-dialog.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogConfig } from '@angular/material/dialog';
+import { StrictlyTypedMatDialogService } from '../strictly-typed-mat-dialog.service';
+import { TestDialogComponent } from './test-dialog.component';
+
+describe('StrictlyTypedMatDialogService', () => {
+    let service: StrictlyTypedMatDialogService;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [
+                TestDialogComponent  // Instead of declarations, import the standalone component.
+            ],
+            providers: [StrictlyTypedMatDialogService]
+        }).compileComponents();
+
+        service = TestBed.inject(StrictlyTypedMatDialogService);
+    });
+
+    it('should open the dialog and return the correct result type', (done) => {
+        const config: MatDialogConfig<{ message: string }> = { data: { message: 'Hello Test' } };
+        const dialogRef = service.open(TestDialogComponent, config);
+
+        dialogRef.close('TestResult');
+
+        dialogRef.afterClosed().subscribe((result) => {
+            expect(result).toBe('TestResult');
+            done();
+        });
+    });
+});
